be0588a70512709fddb60f70d6db78aab38f622f
Jason <benjamin19890721@gmail.com>
2015-10-31 18:06:27 -0700

    Update modify.rb
    Accept other actions parameters

--- lib/pocket/client/modify.rb
+++ lib/pocket/client/modify.rb
@@ -3,8 +3,8 @@ module Pocket
     # http://getpocket.com/developer/docs/v3/modify
     module Modify
       # required params: actions, consumer_key, access_token
-      def modify actions
-        response = connection.post("/v3/send", {actions: actions})
+      def modify params=[]
+        response = connection.post("/v3/send", params)
         response.body
       end
     end
